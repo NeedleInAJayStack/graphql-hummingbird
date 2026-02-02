1d06e07b60faa7c0f340e8e4edf087b1cf804f82
Jay Herron <NeedleInAJayStack@protonmail.com>
2026-02-01 23:29:48 -0800

    docs: Adds example project

---
+++ Examples/HelloWorld/Sources/HelloWorld/HelloWorld.swift
@@ -0,0 +1,71 @@
+import AsyncAlgorithms
+import GraphQL
+import GraphQLHummingbird
+import Hummingbird
+import HummingbirdWebSocket
+import Logging
+
+@main
+struct HelloWorld {
+    static func main() async throws {
+        let schema = try GraphQLSchema(
+            query: GraphQLObjectType(
+                name: "Query",
+                fields: [
+                    "hello": GraphQLField(
+                        type: GraphQLString,
+                        resolve: { _, _, _, _ in
+                            "World"
+                        }
+                    ),
+                ]
+            ),
+            subscription: GraphQLObjectType(
+                name: "Subscription",
+                fields: [
+                    "hello": GraphQLField(
+                        type: GraphQLString,
+                        description: "Emits an updated `World` message every 3 seconds",
+                        resolve: { eventResult, _, _, _ in
+                            eventResult
+                        },
+                        subscribe: { _, _, _, _ in
+                            let clock = ContinuousClock()
+                            let start = clock.now
+                            return AsyncTimerSequence(interval: .seconds(3), clock: ContinuousClock()).map { instant in
+                                "World at \(start.duration(to: instant))"
+                            }
+                        }
+                    ),
+                ]
+            )
+        )
+
+        let router = Router(context: HummingbirdContext.self)
+        router.graphql(schema: schema, config: .init(allowMissingAcceptHeader: true)) { _, _ in
+            GraphQLContext()
+        }
+        router.graphqlSubscribe("graphqlSubscribe", schema: schema) { _, _ in
+            GraphQLContext()
+        }
+        let app = Application(
+            router: router,
+            server: .http1WebSocketUpgrade(webSocketRouter: router)
+        )
+        try await app.runService()
+    }
+
+    struct GraphQLContext: @unchecked Sendable {}
+
+    struct HummingbirdContext: WebSocketRequestContext, RequestContext {
+        var coreContext: Hummingbird.CoreRequestContextStorage
+        var webSocket: HummingbirdWebSocket.WebSocketHandlerReference<HummingbirdContext>
+        var logger: Logging.Logger
+
+        init(source: Hummingbird.ApplicationRequestContextSource) {
+            coreContext = .init(source: source)
+            webSocket = .init()
+            logger = source.logger
+        }
+    }
+}
